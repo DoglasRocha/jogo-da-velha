6589a9b39d2e52fb965db3218d298f14ff2b2a30
DoglasRocha <doglas_rocha@protonmail.ch>
2021-09-21 17:55:21 -0300

    change the player value

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 var player;
 var selectedPlayer = document.getElementById('selected-player');
+var squares = document.getElementsByClassName('square');
 
 const chooseSquare = (id) => {
 	let square = document.getElementById(id);
+	if (square.innerHTML !== '-') return;
 
 	square.innerHTML = player;
 	square.style.color = '#121212';
+
+	if (player === 'x') {
+		player = 'o';
+	} else {
+		player = 'x';
+	}
+
+	changePlayer(player);
 }
 
 const changePlayer = value => {
 	player = value;
 	selectedPlayer.innerHTML = player;
+}
+
+const checkWinner = () => {
+	
+}
+
+const checkSequence = () => {
+
 }
 
 changePlayer('x');
